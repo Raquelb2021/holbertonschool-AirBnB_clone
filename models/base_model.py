90656d937bc34c8d947b98f8c8f79e9ea91b343f
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-06-09 10:31:16 -0700

    Write a class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,51 @@
+#!/usr/bin/python3
+"""class basemodel"""
+import models
+import uuid
+from datetime import datetime
 
+class BaseModel:
+    """Initialization of basemodel class
+     that defines all common attributes/methods for other classes:
+    """
+    def __init__(self):
+        """
+        uuid.uuid4() generate unique id
+
+        created_at assign with the current datetime when an instance is created
+
+        updated_at assign with the current datetime
+        when an instance is created and it will be updated every time you change your object
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """returns a string representation of the object.
+
+        In the case of the BaseModel class
+
+        """
+        return '[{}] ({}) {}'.format(
+                self.__class__.__name__,
+                self.id,
+                self.__dict__
+                )
+
+    def save(self):
+        """save(self): updates the public instance
+        attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """The to_dict method returns a dictionary
+        containing all keys/values of the instance’s
+        """
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
