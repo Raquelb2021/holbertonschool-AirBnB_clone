b96e83f6906ae344929db714f68d2cc42a2505eb
EveBaker <Evebaker0162@gmail.com>
2023-06-09 09:42:49 -0700

    fixing issues

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Console module"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class"""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, _):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, _):
+        """EOF command to exit the program"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
