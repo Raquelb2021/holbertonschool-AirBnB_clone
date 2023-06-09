0899f121cfee5f4535d9d2fe66e2d295597c3a24
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-06-09 16:52:32 -0700

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -20,9 +20,15 @@ class BaseModel:
         when an instance is created and it will be
         updated every time you change your object
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string representation of the object.
