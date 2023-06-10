024910111b10dbfae5ba9c799cf0fd4ae1a51e88
EveBaker <Evebaker0162@gmail.com>
2023-06-09 17:59:51 -0700

    added test file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,34 +1,35 @@
 import json
 from models.base_model import BaseModel
 
-
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        key = obj.__class__.__name__ + "." + obj.id
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
         obj_dict = {}
         for key, obj in self.__objects.items():
             obj_dict[key] = obj.to_dict()
-
-        with open(self.__file_path, "w") as file:
+        with open(self.__file_path, 'w') as file:
             json.dump(obj_dict, file)
 
     def reload(self):
+        """Deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, "r") as file:
+            with open(self.__file_path, 'r') as file:
                 obj_dict = json.load(file)
-
-                for key, value in obj_dict.items():
-                    class_name = value["__class__"]
-                    obj = eval(class_name + "(**value)")
+                for key, obj_data in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+                    obj = BaseModel(**obj_data)
                     self.__objects[key] = obj
         except FileNotFoundError:
             pass
