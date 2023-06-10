1f2022e7710fecd679a1059f9912d41c60cb9ade
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-06-10 11:27:27 -0700

    class states

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+"""class State that inherits from BaseModel"""
+
+
+class State(BaseModel):
+    """initialize class with a public attribute
+    name: string - empty string
+    """
+    name = ''
