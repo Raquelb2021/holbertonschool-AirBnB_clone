2874144b52699694e7674065739d039145b20658
EveBaker <Evebaker0162@gmail.com>
2023-06-09 17:38:08 -0700

    added to base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,12 @@
 import models
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """Initialization of basemodel class
-     that defines all common attributes/methods for other classes:
+    that defines all common attributes/methods for other classes:
     """
     def __init__(self, *args, **kwargs):
         """
@@ -29,6 +30,8 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
+
 
     def __str__(self):
         """returns a string representation of the object.
@@ -47,10 +50,11 @@ class BaseModel:
         attribute updated_at with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """The to_dict method returns a dictionary
-        containing all keys/values of the instance’s
+        containing all keys/values of the instances
         """
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
@@ -58,3 +62,5 @@ class BaseModel:
         new_dict['updated_at'] = self.updated_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
+
+
