2a7c9f8b5ef9873d7dfb82e43642894e78d89e14
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-06-10 17:57:17 -0700

    9. More classes!

--- models/place.py
+++ models/place.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-"""Public class attributes"""
+    """Public class attributes"""
+
     def __init__(self):
-    city_id = City.id
-    user_id = User.id
-    name = ''
-    self.description = 0
-    self.number_rooms = 0
-    self.number_bathrooms = 0
-    self.max_guest = 0
-    self.price_by_night = 0
-    self.latitude = 0.0
-    self.longitude = 0.0
-    self.amenity_ids = {}
+        city_id = City.id
+        user_id = User.id
+        name = ''
+        self.description = 0
+        self.number_rooms = 0
+        self.number_bathrooms = 0
+        self.max_guest = 0
+        self.price_by_night = 0
+        self.latitude = 0.0
+        self.longitude = 0.0
+        self.amenity_ids = {}
