57361bacd6773f793bc77c2814377a3bea90d468
EveBaker <Evebaker0162@gmail.com>
2023-06-11 18:52:42 -0700

    hopefully fixed

--- console.py
+++ console.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Module 6. Console 0.0.1"""
+"""Module for Console 0.0.1, 0.1, 1.0"""
 
-import io
-import sys
 import cmd
 import shlex
 from models import storage
@@ -11,11 +9,11 @@ from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
-    """Class HBNBCommand inherits from cmd.Cmd and defines methods for our command-line application"""
-
-    prompt = '(hbnb) '  # Custom prompt attribute
+    """class HBNB inherits from cmd.Cmd"""
+    prompt = '(hbnb) '
 
     def __init__(self):
+        """ init method to HBNBcommand subclass of cmd.CMD """
         super().__init__()
 
     def do_quit(self, arg):
@@ -31,8 +29,8 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        """Creates a new instance of BaseModel, saves it (to the JSON file), and prints the ID."""
-        args = shlex.split(arg)  # Split the argument string into a list of individual arguments
+        """Creates a new instance of BaseModel"""
+        args = shlex.split(arg) # Split the argument string into a list of individual arguments
         if len(args) == 0:
             print("** class name missing **")
         elif args[0] != "BaseModel":
@@ -43,66 +41,53 @@ class HBNBCommand(cmd.Cmd):
             print(new_instance.id)  # Print the ID of the new instance
 
     def do_show(self, arg):
-        """Prints the string representation of an instance based on the class name and ID."""
-        args = shlex.split(arg)  # Split the argument string into a list of individual arguments
+        """Prints the string representation of an
+        instance"""
+        args = shlex.split(arg) # Split the argument string into a list of individual arguments
         if len(args) == 0:
             print("** class name missing **")
         elif args[0] != "BaseModel":
             print("** class doesn't exist **")
         elif len(args) < 2:
-            print("** instance ID missing **")
+            print("** instance id missing **")
         else:
             instance = storage.get("BaseModel", args[1])  # Get the instance based on class name and ID
             if not instance:
                 print("** no instance found **")
             else:
-                print(instance)  # Print the string representation of the instance
+                print(instance) # Print the string representation of the instance
 
     def do_destroy(self, arg):
-        """Deletes an instance based on the class name and ID."""
+        """Deletes an instance based on the class name and id."""
         args = shlex.split(arg)  # Split the argument string into a list of individual arguments
         if len(args) == 0:
             print("** class name missing **")
         elif args[0] != "BaseModel":
             print("** class doesn't exist **")
         elif len(args) < 2:
-            print("** instance ID missing **")
+            print("** instance id missing **")
         else:
             if not storage.delete("BaseModel", args[1]):
                 print("** no instance found **")
 
     def do_all(self, arg):
-        """Prints all instances of a class, or all instances if no class name is provided."""
+        """Prints all instances of a class"""
         args = shlex.split(arg)  # Split the argument string into a list of individual arguments
         if len(args) > 0 and args[0] != "BaseModel":
             print("** class doesn't exist **")
         else:
-            instances = storage.all("BaseModel" if len(args) > 0 else None)  # Get all instances of a class
+            instances = storage.all("BaseModel" if len(args) > 0 else None) # Get all instances of a class
             for instance in instances.values():
-                print(instance)  # Print each instance
+                print(instance) # Print each instance
 
     def do_update(self, arg):
-        """Updates an instance based on the class name and ID by adding or updating an attribute."""
-        args = shlex.split(arg)  # Split the argument string into a list of individual arguments
+        """Updates an instance based on the class name"""
+        args = shlex.split(arg) # Split the argument string into a list of individual arguments
         if len(args) == 0:
-            print("** class name missing **")
+            print("** class name missing **")  # Set the attribute of the instance
         elif args[0] != "BaseModel":
-            print("** class doesn't exist **")
-        elif len(args) < 2:
-            print("** instance ID missing **")
-        else:
-            instance = storage.get("BaseModel", args[1])  # Get the instance based on class name and ID
-            if not instance:
-                print("** no instance found **")
-            elif len(args) < 3:
-                print("** attribute name missing **")
-            elif len(args) < 4:
-                print("** value missing **")
-            else:
-                setattr(instance, args[2], args[3])  # Set the attribute of the instance
-                instance.save()  # Save the updated instance
-
+            print() # Save the updated instance
 
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()  # Start the command-line interface loop
+    HBNBCommand().cmdloop()
