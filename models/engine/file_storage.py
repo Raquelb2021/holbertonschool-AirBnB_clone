988424fde9aa6891518c493265fb6140b694d876
EveBaker <Evebaker0162@gmail.com>
2023-06-12 14:39:58 -0700

    added imports

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,11 @@ import json
 from models.base_model import BaseModel
 from datetime import datetime
 import models
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """STORE NEW FILES"""
