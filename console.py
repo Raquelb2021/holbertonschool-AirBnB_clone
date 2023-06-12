9cc1faf4fdac1b114dcf77d89a194bf3475604f8
EveBaker <Evebaker0162@gmail.com>
2023-06-12 06:23:28 -0700

    edit

--- console.py
+++ console.py
@@ -96,7 +96,7 @@ class HBNBCommand(cmd.Cmd):
         elif len(args) < 4:
             self.print_error_message("value missing")
         else:
-            instance = storage.get_instance_by_id("BaseModel", args[1])
+            instance = storage.all("BaseModel").get(args[1])
             if instance is None:
                 self.print_error_message("no instance found")
             else:
