62a340a19e6209ae6f301d3469dcd2b1be226851
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-06-12 11:11:07 -0700

    try to change the get_instance_by_id to get()

--- console.py
+++ console.py
@@ -96,7 +96,7 @@ class HBNBCommand(cmd.Cmd):
         elif len(args) < 4:
             self.print_error_message("value missing")
         else:
-            instance = storage.all("BaseModel")
+            instance = storage.all()
             instance_key = "BaseModel.{}".format(args[1])
             intance = instances.get(instance_key)
             if instance is None:
