294e4caad30feea8fe0ff35b046b6f88bf5283da
Raquelb2021 <raquelmcbezerra@gmail.com>
2023-06-13 17:49:44 -0700

    test storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,62 +1,43 @@
 #!/usr/bin/python3
-""" Test file_storage module """
+"""Tests for the file storage"""
 
 import unittest
-from models import BaseModel
-from file_storage import FileStorage
-import os
-
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 class TestFileStorage(unittest.TestCase):
-    """ Class to test file storage engine an the methods it holds """
     def setUp(self):
-        # Create a new FileStorage object before each test
-        self.file_storage = FileStorage()
-        # Create a new instance of the base model
-        self.test_object = BaseModel()
-        # Store this instance in the FileStorage
-        self.file_storage.new(self.test_object)
-        # generate the key for this instance
-        self.key = "{}.{}".format(
-            self.test_object.__class__.__name__, self.test_object.id)
+        self.fs = FileStorage()
+        self.fs._FileStorage__objects.clear()
 
     def test_all(self):
-        """ Test the 'all' method of file storage"""
-        self.assertEqual(self.file_storage.all()[self.key], self.test_object)
+        self.assertEqual(self.fs.all(), {})
 
     def test_new(self):
-        """ Test the 'new' method of File Storage class """
-        self.assertIn(self.key, self.file_storage.all())
-        self.assertEqual(self.file_storage.all()[self.key], self.test_object)
+        bm = BaseModel()
+        self.fs.new(bm)
+        key = "BaseModel.{}".format(bm.id)
+        self.assertIn(key, self.fs.all())
+        self.assertEqual(self.fs.all()[key], bm)
 
-    def test_save_and_reload(self):
-        """ test the 'save and 'reload' methods of FIle Storage Classs"""
-        self.file_storage.save()
+    def test_save(self):
+        bm = BaseModel()
+        self.fs.new(bm)
+        self.fs.save()
+        with open(self.fs._FileStorage__file_path, 'r') as f:
+            content = f.read()
+            key = "BaseModel.{}".format(bm.id)
+            self.assertIn(key, content)
 
-        self.file_storage.clear()
-        self.assertNotIn(self.key, self.file_storage.all())
+    def test_reload(self):
+        bm = BaseModel()
+        self.fs.new(bm)
+        self.fs.save()
+        key = "BaseModel.{}".format(bm.id)
+        del self.fs._FileStorage__objects[key]
+        self.assertNotIn(key, self.fs.all())
+        self.fs.reload()
+        self.assertIn(key, self.fs.all())
 
-        self.file_storage.reload()
-
-        # Check if the data is correctly loaded
-        self.assertIn(self.key, self.file_storage.all())
-        self.assertEqual(
-            self.test_object.to_dict(),
-            self.file_storage.all()[self.key].to_dict())
-
-    def test_file_path(self):
-        """ Test if file path is set correctly """
-        self.assertEqual(
-            self.file_storage._FileStorage__file_path,
-            "./file.json")
-
-    def tearDown(self):
-        """tears down"""
-        try:
-            os.remove(self.file_storage. FileStorage__file_path)
-        except FileNotFoundError:
-            pass
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
